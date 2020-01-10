bbc8536efd542a0afa06343decbadacb34546cb0
Michael Dokolin <mikhail.dokolin@bloomreach.com>
2020-01-10 15:26:30 +0100

    ENT-1519 Add project configuration

---
+++ README.md
@@ -0,0 +1,20 @@
+# brXM + Next.js = ♥️
+
+Example Next.js SPA using the Bloomreach Experience [React SDK](https://www.npmjs.com/package/@bloomreach/react-sdk).
+The app uses unversal framework [Next.js](https://github.com/zeit/next.js) for creating isomorphic React applications.
+
+## Configuration
+
+Copy `.env.dist` file to `.env` and customize it to contain a correct `PUBLIC_URL` path, for example:
+```
+PUBLIC_URL=http://localhost:3000
+```
+
+Then, build and run the Next.js app as followed:
+
+```bash
+npm install
+npm run dev
+```
+
+The SPA should now be accessible at <http://localhost:3000>.
---
+++ pages/_document.tsx
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2020 Hippo B.V. (http://www.onehippo.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Document, { Head, Main, NextScript } from 'next/document';
+
+export default class DefaultDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="Example Next.js SPA for Bloomreach Experience" />
+
+          <title>brXM + Next.js = ♥️</title>
+
+          <link rel="shortcut icon" type="image/png" href={`${process.env.PUBLIC_URL}/favicon.png`} sizes="64x64" />
+          <link rel="stylesheet" media="screen"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+            crossOrigin="anonymous" />
+          <link rel="stylesheet" href={`${process.env.PUBLIC_URL}/theme.css`} media="screen" />
+
+          <Head />
+        </head>
+        <body>
+          <div id="root">
+            <Main />
+          </div>
+
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
